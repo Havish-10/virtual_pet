f12a87ae82c947566de772f07bf7241272d67fb9
Havish-10 <patelhavish10@gmail.com>
2023-02-20 08:28:54 +0000

    Improved UI on minigame and properly functional.

--- client/minigames.js
+++ client/minigames.js
@@ -2,7 +2,7 @@ let score = 0;
 let livesLeft = 7;
 const words = ['jump', 'throw', 'fall', 'free', 'tomagatchi', 'feed', 'forest', 'weird', 'jack', 'hangman', 'falling', 'Simpsons', 'Family Guy', 'Free Fall', 'Healthy Pet', 'Good Owner', 'Bad Owner', 'Jumping Jack'];
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const gusess = [];
+const guesses = [];
 let mistakes = -1;
 let keyPressed = {};
 const selectWord = Array.from(words[Math.floor(Math.random() * words.length)]);
@@ -10,22 +10,35 @@ const hangmanArr = new Array(selectWord.length).fill('_');
 const lowerCased = selectWord.map(selectWord => selectWord.toLowerCase());
 
 function whichKey(e) {
-  const p = document.querySelector('#L');
-  p.textContent = e.key;
   console.log(keyPressed);
   score = Math.round(e.timeStamp / 1000);
   for (let i = 0; i <= alphabet.length; i++) {
     if (e.key === alphabet[i]) {
       keyPressed = e.key;
-      if (!gusess.includes(keyPressed)) {
-        gusess.push(keyPressed);
+      if (!guesses.includes(keyPressed)) {
+        guesses.push(keyPressed);
+      } else if (guesses.includes(keyPressed)){
+        const keypress = document.querySelector('#keypress');
+        keypress.textContent = 'You have already pressed the ' + e.key + ' key Silly!';
       }
     }
   }
   hangman();
+  showGuesses();
+}
+
+function showGuesses(){
+  const guessesMade = document.querySelector("#guessed_letters");
+  guessesMade.textContent = guesses.join("   ").toUpperCase();
 }
 
 function hangman() {
+  const instruct = document.querySelector('#instructions');
+  instruct.classList.add('hidden');
+
+  const guessesShow = document.querySelector("#guessed_letters");
+  guessesShow.classList.remove("hidden");
+
   const unblur = document.querySelectorAll('*');
   for (const elements of unblur) {
     elements.classList.remove('blurred');
@@ -67,13 +80,13 @@ function gameStart() {
 }
 
 function gameEnd() {
-  const endGame = document.querySelector('#end');
+  const endGame = document.querySelector('#lose');
   if (livesLeft > 0) {
     endGame.classList.toggle('win');
     endGame.textContent = 'You WIN!';
   } else if (livesLeft <= 0) {
     livesLeft = 0;
-    endGame.classList.toggle('death');
+    endGame.classList.remove('hidden');
     endGame.textContent = 'You DIED!';
   }
 }
